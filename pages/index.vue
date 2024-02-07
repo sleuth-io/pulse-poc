981944a8c9c285c07ca0cd091ad5c3fc50b79719
mzgajner <mato@zgajner.com>
2024-02-07 10:46:51 +0100

    Save definition with new version

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,21 @@ const reviewDefinitionVersion = 1
 
 const database = useState('db', () => null)
 
-const definition = computed(() =>
-  database.value?.workspaces[0].folders[0].reviewDefinitions.find(
+const definition = ref(JSON.parse(JSON.stringify(database.value?.workspaces[0].folders[0].reviewDefinitions.find(
     rd => rd.slug === reviewDefinitionSlug && rd.version === reviewDefinitionVersion
-  )
-)
+  ))))
+
 
 function addField() {
   definition.value.schema.push({
     title: 'New widget',
     slug: (Math.random() + 1).toString(36).substring(7)
   })
+}
+
+function save() {
+  definition.value.version++
+  database.value?.workspaces[0].folders[0].reviewDefinitions.push(definition.value)
 }
 
 </script>
@@ -24,6 +28,9 @@ function addField() {
       <template #content>
         <InputText v-model="field.title"></InputText></template>
     </Card>
-    <Button label="+ Add field" class="mt-8" @click="addField"/>
+    <Button label="+ Add field" class="mt-8" @click="addField" severity="secondary"/>
+    <hr class="mt-8">
+    <Button label="Save template" class="mt-8" @click="save"/>
+
   </div>
 </template>
