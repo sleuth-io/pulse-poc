12e1ace4fd20895ef6521aff7ea432fe158a2e5d
pr1moz <pr1moz@users.noreply.github.com>
2024-02-07 14:21:10 +0100

    Fix review create margins

--- pages/[workspaceSlug]/[folderSlug]/[definitionSlug]/review/_create.vue
+++ pages/[workspaceSlug]/[folderSlug]/[definitionSlug]/review/_create.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-const reviewDate = '2021-01-01';
+const reviewDate = "2021-01-01";
 
-const route = useRoute('workspaceSlug-folderSlug-definitionSlug-review-_create')
-const router = useRouter()
+const route = useRoute(
+  "workspaceSlug-folderSlug-definitionSlug-review-_create"
+);
+const router = useRouter();
 
-const db = useState('db')
+const db = useState("db");
 
-const workspace = db.value.workspaces.find((workspace) => workspace.slug === route.params.workspaceSlug)
-const definition = workspace.folders.find((folder) => folder.slug === route.params.folderSlug).reviewDefinitions.sort((a, b) => b.version - a.version).find((definition) => definition.slug === route.params.definitionSlug)
+const workspace = db.value.workspaces.find(
+  (workspace) => workspace.slug === route.params.workspaceSlug
+);
+const definition = workspace.folders
+  .find((folder) => folder.slug === route.params.folderSlug)
+  .reviewDefinitions.sort((a, b) => b.version - a.version)
+  .find((definition) => definition.slug === route.params.definitionSlug);
 
-const value = ref<Record<string, number>>({})
+const value = ref<Record<string, number>>({});
 
 async function createReview() {
   definition.reviews.push({
@@ -18,23 +25,29 @@ async function createReview() {
     values: value,
   });
 
-  await router.push({ name: 'workspaceSlug-folderSlug-definitionSlug' })
+  await router.push({ name: "workspaceSlug-folderSlug-definitionSlug" });
 }
 </script>
 
 <template>
   <div class="flex flex-col">
-    <label>
-      Review date:
-      <input v-model="reviewDate" type="date" />
-    </label>
+    <div>
+      <div>
+        <p>Review date:</p>
+        <InputText v-model="reviewDate" type="date" class="w-52 mb-3" />
+      </div>
 
-    <template v-for="field in definition.schema">
-      <label>
-        {{ field.title }}:
-        <input v-model="value[field.id]" type="number" />
-      </label>
-    </template>
-    <Button @click="createReview">Create</Button>
+      <template v-for="field in definition.schema">
+        <div>
+          <p>{{ field.title }}:</p>
+          <InputText
+            v-model="value[field.id]"
+            type="number"
+            class="w-52 mb-3"
+          />
+        </div>
+      </template>
+      <Button @click="createReview" label="Create" />
+    </div>
   </div>
 </template>
