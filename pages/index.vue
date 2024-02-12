896fb65b65a77525718a976b765672677a6478a1
mzgajner <mato@zgajner.com>
2024-02-12 17:30:45 +0100

    Land on first workspace by default

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
-<template>
-  <h1>
-    Welcome to Pulse
-  </h1>
-</template>
+<script setup lang="ts">
+const db = useDatabase()
+const workspaceSlug = db.value.workspaces[0].slug
+navigateTo({ name: 'workspaceSlug', params: { workspaceSlug } })
+</script>
